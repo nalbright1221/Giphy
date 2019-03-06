07557f10b8deb7fa7c22edc46b75b3a914a5c6a9
Nicole <nalbrigh@kent.edu>
2019-03-06 01:01:22 -0500

    half way done with basic requirments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,16 +2,16 @@
 var topics = ['angry', ' bored', ' disappointed', ' drunk', ' embarassed', ' excited', ' frustrated', ' happy', ' hungry', ' inpsired', ' lonely', ' love', ' nervous', ' relaxed', ' sad', ' sassy', ' scared', ' shocked', ' sick', ' tired'];
 
 function buttons() {
-$("#buttons").empty();
+    $("#buttons").empty();
 
-//create buttons using topics in array 
-for (var i = 0; i < topics.length; i++){
-    var newButtons = $("<button>");
-    // research adding a class and an attr to buttons - 
-    newButtons.text(topics[i]);
-    newButtons.addClass("clickMeForGif")
-    $("#buttons").append(newButtons);
-}
+    //create buttons using topics in array 
+    for (var i = 0; i < topics.length; i++) {
+        var newButtons = $("<button>");
+        // research adding a class and an attr to buttons - 
+        newButtons.text(topics[i]);
+        newButtons.addClass("clickMeForGif")
+        $("#buttons").append(newButtons);
+    }
 }
 //call buttons function to generate buttons listed in array 
 buttons();
@@ -19,31 +19,55 @@ buttons();
 //create a new button and append it into id buttons 
 
 //this click even runs when user hits sumbit or enter after typing in new button value 
-$("#userSubmit").on("click", function(event) {
+$("#userSubmit").on("click", function (event) {
     //prevents page from reloading everytime sumbit button is  clicked 
     event.preventDefault();
-//variable that takes the value of new button user created 
-var newEmotion = $("#newEmotionButton").val().trim();
+    //variable that takes the value of new button user created 
+    var newEmotion = $("#newEmotionButton").val().trim();
 
-//pushes new emotion button to our button topics array
-topics.push(newEmotion);
+    //pushes new emotion button to our button topics array
+    topics.push(newEmotion);
 
-//call buttons function
-buttons();
+    //call buttons function
+    buttons();
 
 });
 
-
 //create function that allows the user to click a button  for each string in the  array 
+$(".clickMeForGif").on("click", function () {
+    var a = $(this).text();
 
-function buttonClick() {
-    $(".clickMeforGif").on("click");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + a + "&api_key=5OMJTXCR27pKXP4E9xeUel3ubAzaXQu8";
 
-}
-buttons();
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
 
-function pausePlay() {
-}
+        .then(function (response) {
+
+            var results = response.data;
+            console.log(response);
+
+            for (var i = 0; i < results.length; i++) {
+
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+
+                    var gifDiv = $("<div>");
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var personImage = $("<img>");
+                    personImage.attr("src", results[i].images.fixed_height.url);
+                    gifDiv.addClass("pausePlay");
+                    gifDiv.append(p);
+                    gifDiv.append(personImage);
+                    $("#gifSection").prepend(gifDiv);
+
+                }
+            }
+        });
+});
+
 //grab 10 static non animated gifs 
 
 //place those gifs onto page
@@ -52,22 +76,20 @@ function pausePlay() {
 //change still image to animated gif 
 //when user clicks again -changes to still image
 
-//display gif ratings 
+$(".pausePlay").on("click", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 
-//take form value and generate a new button 
-// add it to topics array 
-
-//create function call that takes new topic and remakes the buttons on the page 
 
 
-// gify API KEY = "5OMJTXCR27pKXP4E9xeUel3ubAzaXQu8"
-
-// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-// animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   })
-
-//   .then(function(response) {})
